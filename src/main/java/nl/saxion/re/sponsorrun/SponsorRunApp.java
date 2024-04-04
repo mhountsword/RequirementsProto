f4763151be0a002c9aa229d02f02aee05eee12eb
mhountsword <XLmaarten@gmail.com>
2024-04-04 22:02:00 +0200

    some more dashboard work

--- src/main/java/nl/saxion/re/sponsorrun/SponsorRunApp.java
+++ src/main/java/nl/saxion/re/sponsorrun/SponsorRunApp.java
@@ -2,22 +2,17 @@ package nl.saxion.re.sponsorrun;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import nl.saxion.re.sponsorrun.data.Data;
 import nl.saxion.re.sponsorrun.util.WindowHelper;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SponsorRunApp extends Application {
-    @Override
     public void start(Stage stage) throws IOException {
-
-        // read all data from disk
-        Data.updateFromDisk();
-
         // start the main menu window
         WindowHelper.openWindow("my-dashboard.fxml", "Sponsor Run App", 750, 750, stage);
     }
-
     public static void main(String[] args) {
         launch();
     }
--- src/main/java/nl/saxion/re/sponsorrun/controllers/MyDashboard.java
+++ src/main/java/nl/saxion/re/sponsorrun/controllers/MyDashboard.java
@@ -1,5 +1,18 @@
 package nl.saxion.re.sponsorrun.controllers;
 
-public class MyDashboard {
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import nl.saxion.re.sponsorrun.util.WindowHelper;
 
+public class MyDashboard {
+    @FXML private Pane paneOne;
+    @FXML
+    private void onSponsorAdded(){ //button to add new sponsor
+        WindowHelper.openWindow("test.fxml", "Hello!", 300, 300); //test, can remove
+    }
+
+    @FXML
+    private void sponsorButton(){
+        WindowHelper.openWindow("sponsor-info.fxml", "Sponsor Info", 750, 750);
+    }
 }
