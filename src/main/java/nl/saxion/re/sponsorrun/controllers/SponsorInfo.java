20355d3bb96113ff7238b82cab87c3792dff9089
mhountsword <XLmaarten@gmail.com>
2024-04-07 14:50:12 +0200

    last changes

--- src/main/java/nl/saxion/re/sponsorrun/controllers/SponsorInfo.java
+++ src/main/java/nl/saxion/re/sponsorrun/controllers/SponsorInfo.java
@@ -1,5 +1,27 @@
 package nl.saxion.re.sponsorrun.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import nl.saxion.re.sponsorrun.util.WindowHelper;
+
 public class SponsorInfo {
-    //TODO: add functionality
+    @FXML
+    Button dashboardButton;
+    @FXML
+    public void dashboardButtonOnClick(ActionEvent actionEvent) {
+        goToDashboard();
+    }
+    @FXML
+    public void saveButtonOnClick(){
+        goToDashboard();
+    }
+    @FXML
+    public void cancelButtonOnClick(){
+        goToDashboard();
+    }
+
+    private void goToDashboard() {
+        WindowHelper.closeWindow(dashboardButton);
+    }
 }
